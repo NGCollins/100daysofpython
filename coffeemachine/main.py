46017aa6f9a3cd2c1b114e856a91bf8ee405267a
nick <nicholasg.collins@gmial.com>
2021-12-01 22:09:51 +0000

    pushing code up before bed lol

--- coffeemachine/main.py
+++ coffeemachine/main.py
@@ -1,17 +1,69 @@
-from resources import MENU, resources
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+COINS={"quarter":0.25,"dimes": 0.10,"nickles":0.05,"pennies":0.01}
+
+def resource_check(resource_order, resource_stock,resource):
+    if resource_order < resource_stock:
+        print(f"Sorry, there is no a not {resource}")
+
+
+# TODO number 2 check resources sufficient to make drink order
 
 def coffee_machine():
+    water_amount_left = resources['water']
+    milk_amount_left = MENU['milk']
+    coffee_amount_left = MENU['coffee']
+    money_amount_collected = 0
+    money_amount_inserted = 0
     users_coffee_choice = input("What would you like? (espresso/latte/cappuccino):")
     if users_coffee_choice == "espresso":
-        espresso_cost = MENU.get("cost")
-
+        espresso_cost = MENU['espresso']["cost"]
+        print(espresso_cost)
+        # TODO  number 3 make a check resource function
+        coffee_requirements = MENU['espresso']["ingredients"]['coffee']
+        water_requirements = MENU['espresso']["ingredients"]['water']
+        print(f"{coffee_requirements} {water_requirements}")
+        resource_check(coffee_requirements, coffee_amount_left, "Coffee")
+        resource_check(water_requirements, water_amount_left, "Water")
+        if money_amount_inserted < espresso_cost:
+            print(f"Sorry that's not enough money. Money refunded.")
 
     elif users_coffee_choice == "off":
         return
     elif users_coffee_choice == "report":
-        water_amount = resources.get("water")
-        print(water_amount)
+
+        print(
+            f"Water: {water_amount_left}ml\nMilk: {milk_amount_left}ml\nCoffee: {coffee_amount_left}g\nMoney: ${money_amount_collected}")
 
 
 coffee_machine()
