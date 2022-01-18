5844964f08003266f7859c59c4232dd5b64eecb7
nick <nicholasg.collins@outlook.com>
2022-01-18 09:37:58 +0000

    push all

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
+
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+
 
 class Snake:
 
@@ -21,9 +23,15 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset_snake(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
-
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
